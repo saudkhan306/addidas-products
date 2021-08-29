ccf0fb4148dc1076a425c611e2a8398665c7ef56
umeranjum17 <umeranjum17@gmail.com>
2021-08-30 00:57:28 +0400

    Cart and wishlist page done

--- src/componants/addidas/Card.js
+++ src/componants/addidas/Card.js
@@ -1,50 +1,58 @@
 import React, { useState } from 'react'
 import { Link, useParams } from 'react-router-dom';
-function Card({data}) {
-    const [classActive, setClassActive] = useState(false);
-    const {productFullID} = useParams();
+
+function Card({data, wishlist, cart, updateCart, updateWishlist}) {
     const [imageIndex, setImageIndex] = useState(0)
-    const toggleActive = (e) => {
-        const id = e.target.id;
-        console.log(id);
-        setClassActive(!classActive)
-    };
-    const handleSetImageIndex = (index) => {
-        setImageIndex(index)
-    } 
-    return <div className="cardContainer">
-        <span className={classActive ? "wishlistButton active" : "wishlistButton"} onClick={(e) => toggleActive(e)}>
+    const handleSetImageIndex = (index) => setImageIndex(index) 
+    const classActive = wishlist.find(wishData => wishData.productId === data.productId)
+    const addToCartActive = cart.find(cartData => cartData.productId === data.productId)
+
+    return <div className={addToCartActive ? "cardContainer active" : "cardContainer"}>
+        <span id={data.productId} className={classActive ? "wishlistButton active" : "wishlistButton"} onClick={(e) => updateWishlist(e.target.id)}>
             <i id={data.productId} className={ classActive ? "fas fa-heart" : "far fa-heart"}></i>
         </span>
         <div className="imageBlock">
-            <span className="listingPrice">
-                Actual Price: <span>{"$" + data.listingPrice}</span>
-            </span>
-            <span className="priceLabel">{"$" + data.salePrice}</span>
-            <span className="discountLabel">{data.discount + "%"}</span>
-            <Link to={`products:?${data.productId}`}><img src={data.images[imageIndex]} alt="" /></Link>
+            <div className="priceCont">
+                <span className="listingPrice">
+                    Actual Price: <span>&#8377;{data.listingPrice}</span>
+                </span>
+                <span className="priceLabel">&#8377;{data.salePrice}</span>
+            </div>
+            <span className={addToCartActive ? "discountLabel active" : "discountLabel"}>{data.discount + "%"}</span>
+            <Link to={`products/${data.productId}`}><img src={data.images[imageIndex]} alt="" /></Link>
         </div>
         <div className="p_textBlock">
             <div className="sizesBlock" style={{display: "block"}}>
                 <ul className="subImages"> 
                     {data.images.map((image, index) => 
-                    <li className={imageIndex==index?'active':""} key={index}><img src={image} onMouseOver={()=> handleSetImageIndex(index)} alt="" /></li> )}
+                    <li className={imageIndex===index?'active':""} key={index}>
+                        <img src={image} onMouseOver={()=> handleSetImageIndex(index)} alt="" />
+                    </li> )}
                 </ul>
             </div>
             <div className="d-flex flex-column justify-content-between" style={{minHeight: "127px"}}>
                 <div className="topBlock">
-                    <Link to={`products:?${data.productId}`}>
+                    <Link to={`products/${data.productId}`}>
                         <div className="p_name mb-2 text-black">{data.productName}</div>
                     </Link>
                     <div className="p_category">Brand: {data.brand}</div>
-                </div>
-                <div className="bottomBlock d-flex justify-content-between align-items-center">
-                    <a href={data.URL} target="_blank" className="btn btn-dark btn-sm px-3 btn-outline-hover">Order Now</a>
-                    <div className="d-flex align-items-center" style={{fontSize: "14px"}}>
+                    <div className="d-flex align-items-center mt-1" style={{fontSize: "14px"}}>
                         <p className="mr-3 mb-0"><i className="fas fa-star text-warning"></i> <strong>{data.rating}</strong></p>
                         <span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
                         <p className="mr-3 mb-0"><i className="fas fa-comment-alt text-secondary"></i> <strong>{data.reviews}</strong></p>
                     </div>
+                </div>
+                <div className="bottomBlock d-flex justify-content-between align-items-center mt-2">
+                    <a href={data.URL} target="_blank" className="btn mr-3 btn-dark btn-sm px-3">Order Now</a>
+                    <button 
+                        id={data.productId} 
+                        onClick={(e) => updateCart(e.target.id) } 
+                        target="_blank" 
+                        className={addToCartActive ? "btn btn-danger btn-sm px-3" : "btn btn-dark btn-sm px-3"} >
+                        {/* <i className={addToCartActive ? "fas fa-minus" : "fas fa-plus"}></i>  */}
+                        {/* { addToCartActive ? <i className="fas fa-shopping-cart"></i> : "Add to Cart" } */}
+                        {addToCartActive ? "Remove from Cart" : "Add to Cart"}
+                    </button>
                 </div>
             </div>
         </div>
